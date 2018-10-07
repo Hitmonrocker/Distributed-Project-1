1af8ef2f5153a5c731651468d8e57caedf94b0e0
Hitmonrocker <vinstert@comcast.net>
2018-10-07 14:44:10 -0400

    Add files via upload

--- gRPC-BulletinBoard-Project-master/src/main/java/com/bulletinboard/BulletinBoardServer.java
+++ gRPC-BulletinBoard-Project-master/src/main/java/com/bulletinboard/BulletinBoardServer.java
@@ -1,10 +1,35 @@
 package com.bulletinboard;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BulletinBoardServer {
+    Map<String,String> Board = new HashMap<String, String>();
 
-	public static void main(String[] args) {
-		
+	public void PUT(String title , String body){
+            Board.put(title,body);
+    }
 
-	}
+    public String GET(String title){
+        if(Board.containsKey(title)){
+            return Board.get(title);
+        }
+        else {
+            return "There is no posting named " + title;
+        }
+    }
 
+    public void DELETE(String title){
+       if(Board.containsKey(title)){
+           Board.remove(title);
+       }
+
+    }
+
+    public Map<String,String> LIST(String title , String body){
+       return Board;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
