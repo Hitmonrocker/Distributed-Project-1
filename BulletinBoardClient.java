fb3407228b8cd355baf2d9cffec9c7dd7db819e7
Hitmonrocker <vinstert@comcast.net>
2018-10-07 15:15:52 -0400

    Add files via upload

---
+++ BulletinBoardClient.java
@@ -0,0 +1,26 @@
+package com.bulletinboard;
+import java.io.IOException;
+import java.net.Socket;
+public class BulletinBoardClient {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		String hostname = args[0];
+		int portNumber = Integer.parseInt(args[1]);
+		Socket conn = new Socket(hostname,portNumber);
+
+	}
+
+	public void ack(String ack)
+	{
+		System.out.println(ack);
+	}
+	public void error(String errno)
+	{
+		System.out.println(errno);
+	}
+	public void print(String title, String message){
+		System.out.println(title + " : "+ message);
+	}
+
+}
---
+++ BulletinBoardServer.java
@@ -0,0 +1,45 @@
+package com.bulletinboard;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BulletinBoardServer {
+    Map<String,String> Board = new HashMap<String, String>();
+
+	public void PUT(String title , String body){
+            Board.put(title,body);
+            //call ack
+    }
+
+    public void  GET(String title){
+        if(Board.containsKey(title)){
+            //call printBoard.get(title);
+        }
+        else {
+
+            // call errrno "There is no posting named ";
+        }
+    }
+
+    public void DELETE(String title){
+       if(Board.containsKey(title)){
+           Board.remove(title);
+           //call ack
+       }
+       //call errno
+    }
+
+    public void LIST(){
+        for (String S:Board.keySet()) {
+            //call print(S+" : "+Board.get(S));
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        int portNumber = Integer.parseInt(args[0]);
+        ServerSocket serverSocket = new ServerSocket(portNumber);
+        Socket clientSocket = serverSocket.accept();
+    }
+}
