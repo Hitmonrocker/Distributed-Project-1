2d57bc5e778aeacd4a4b83342739765bb6e43502
Matthew A. Mann <mam1010@ls.cs.rutgers.edu>
2018-10-15 14:01:51 -0400

    Function Overrides

--- gRPC-BulletinBoard-Project-master/src/main/java/com/bulletinboard/BulletinBoardService.java
+++ gRPC-BulletinBoard-Project-master/src/main/java/com/bulletinboard/BulletinBoardService.java
@@ -1,6 +1,21 @@
 package com.bulletinboard;
 
-public class BulletinBoardService extends BULLETINGrpc{
-	
+public abstract class BulletinBoardService extends BULLETINGrpc.BULLETINImplBase{
+	@Override
+	public void gETS(GET request,
+	        io.grpc.stub.StreamObserver<GET> responseObserver) {
+			return;
+	}
     
+	@Override
+	public void lISTS(LIST request,
+	        io.grpc.stub.StreamObserver<LIST> responseObserver) {
+			return;
+	}
+	
+	@Override
+	public void pOST(PUT request,
+	        io.grpc.stub.StreamObserver<PUT> responseObserver) {
+			return;
+	}
 }
